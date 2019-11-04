20ed9d6d35a44f3d25b3c5731b93456671a1c192
Alexander Repeta <alexander.repeta@gmail.com>
2019-11-05 01:01:32 +0200

    Adds browserlist. Started working on css task

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const { src, dest, series, parallel, watch } = require('gulp');
 const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
-const csso = require('gulp-csso');
+const cssnano = require('cssnano');
 const gcmq = require('gulp-group-css-media-queries');
 const del = require('del');
 const htmlmin = require('gulp-htmlmin');
@@ -22,14 +22,21 @@ const server = require('browser-sync').create();
 const newer = require('gulp-newer');
 const size = require('gulp-size');
 const sourcemaps = require('gulp-sourcemaps');
+const assets = require('postcss-assets');
+const usedcss = require('usedcss');
 
 const paths = {
   src: {
     html: 'src/*.html',
-    css: 'src/sass/styles.scss',
+    css: 'src/sass/main.scss',
     js: 'src/js/**/*.js',
     images: 'src/images/**/*',
     fonts: 'src/fonts/**/*',
+  },
+  watch: {
+    html: 'src/**/*.html',
+    css: 'src/sass/**/*.scss',
+    js: 'src/js/**/*.js',
   },
   build: {
     html: 'build/',
@@ -39,6 +46,97 @@ const paths = {
     fonts: 'build/fonts',
   },
 };
+
+const css = () => {
+  return (
+    src(paths.src.css)
+      // .pipe(plumber())
+      // TODO: prod only
+      // .pipe(sourcemaps.init())
+      // .pipe(
+      //   stylelint({
+      //     reporters: [{ formatter: 'string', console: true }],
+      //   }),
+      // )
+      .pipe(
+        sass({
+          sourceMap: true,
+          // FIXME: image append not working
+          imagePath: '/images/',
+          precision: 3,
+          errLogToConsole: true,
+        }).on('error', sass.logError),
+      )
+      // TODO: Prod only
+      // .pipe(gcmq())
+      .pipe(
+        postcss([
+          // TODO: enable after html task is ready
+          // usedcss({
+          //   html: ['index.html'],
+          // }),
+          assets({
+            // FIXME: image append not working
+            loadPaths: ['/images/'],
+            basePath: paths.build.css,
+          }),
+          // TODO: prod only maybe
+          autoprefixer(),
+          // cssnano,
+        ]),
+      )
+      // TODO: prod only
+      // .pipe(sourcemaps.write())
+      .pipe(size({ showFiles: true }))
+      // TODO: prod only
+      // .pipe(rename('styles.min.css'))
+      .pipe(dest(paths.build.css))
+  );
+  // .pipe(server.stream())
+};
+
+exports.css = css;
+
+// const styles = () => {
+//   return src('src/sass/styles.scss')
+//     .pipe(plumber())
+// .pipe(
+//   stylelint({
+//     reporters: [{ formatter: 'string', console: true }],
+//   }),
+// )
+//     .pipe(sass())
+// .pipe(
+//   postcss([
+//     autoprefixer({
+//       browsers: ['last 2 versions'],
+//     }),
+//   ]),
+// )
+//     .pipe(gcmq())
+//     .pipe(csso())
+//     .pipe(rename('styles.min.css'))
+//     .pipe(dest('build/css'))
+//     .pipe(server.stream());
+// };
+
+const images = () => {
+  return src(paths.src.images)
+    .pipe(newer(paths.build.images))
+    .pipe(
+      imagemin([
+        imagemin.jpegtran({ progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [{ removeViewBox: false }, { cleanupIDs: false }],
+        }),
+      ]),
+    )
+    .pipe(size({ showFiles: true }))
+    .pipe(dest(paths.build.images));
+};
+
+exports.images = images;
 
 // const html = () => {
 //   return src('src/*.html')
@@ -87,24 +185,6 @@ const paths = {
 //     .pipe(rename('sprite.svg'))
 //     .pipe(dest('build/images'));
 // };
-
-const images = () => {
-  return src(paths.src.images)
-    .pipe(newer(paths.build.images))
-    .pipe(
-      imagemin([
-        imagemin.jpegtran({ progressive: true }),
-        imagemin.optipng({ optimizationLevel: 5 }),
-        imagemin.svgo({
-          plugins: [{ removeViewBox: false }, { cleanupIDs: false }],
-        }),
-      ]),
-    )
-    .pipe(size({ showFiles: true }))
-    .pipe(dest(paths.build.images));
-};
-
-exports.images = images;
 
 // const imagesOld = () => {
 //   return src(['src/images/**/*.{png,jpg,jpeg,svg}', '!src/images/icons/**/*'])
