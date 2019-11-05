5937356ebeeab8cdea7dc032746f3450b53e76b4
Alexander Repeta <alexander.repeta@gmail.com>
2019-11-05 22:34:14 +0200

    Adds cache busting and gh-pages deployment script

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,18 +1,24 @@
 const gulp = require('gulp');
 const rigger = require('gulp-rigger');
 const htmlmin = require('gulp-htmlmin');
+const cachebust = require('gulp-cache-bust');
 const mode = require('gulp-mode')();
 const paths = require('../paths');
 
-const htmlMinConfig = {
+const htmlminConfig = {
   collapseWhitespace: true,
+};
+
+const cachebustConfig = {
+  type: 'timestamp',
 };
 
 const html = () => {
   return gulp
     .src(paths.src.html)
     .pipe(rigger())
-    .pipe(mode.production(htmlmin(htmlMinConfig)))
+    .pipe(mode.production(htmlmin(htmlminConfig)))
+    .pipe(mode.production(cachebust(cachebustConfig)))
     .pipe(gulp.dest(paths.build.html));
 };
 
