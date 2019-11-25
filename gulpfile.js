3080e019215d95fb1ecd60249ae2440ed4fa8083
Alexander Repeta <alexander.repeta@gmail.com>
2019-11-25 18:06:27 +0200

    bugfix: added inject task after every html change

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,10 @@ const serve = () => {
 };
 
 const watcher = done => {
-  watch(paths.watch.html).on('change', series(tasks.html, browserSync.reload));
+  watch(paths.watch.html).on(
+    'change',
+    series(tasks.html, tasks.inject, browserSync.reload),
+  );
   watch(paths.watch.css).on('change', series(tasks.css, browserSync.reload));
   watch(paths.watch.js).on('change', series(tasks.scripts, browserSync.reload));
   watch(paths.watch.images, tasks.images);
