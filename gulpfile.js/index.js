1264bd10d275cfe8299493c37c35794bdda9c174
Alexander Repeta <alexander.repeta@gmail.com>
2021-05-15 15:25:25 +0300

    feat: add build gulp task

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -5,12 +5,16 @@ const paths = require('./paths');
 const { serve, browserSync } = require('./serve');
 const html = require('./html');
 const styles = require('./styles');
+const images = require('./images');
 const clean = require('./clean');
 
 const watch = done => {
   gulp.watch(paths.watch.html, gulp.series(html, browserSync.reload));
-  gulp.watch(paths.src.css, gulp.series(styles, browserSync.reload));
+  gulp.watch(paths.watch.css, gulp.series(styles, browserSync.reload));
+  gulp.watch(paths.watch.images, gulp.series(images, browserSync.reload));
   done();
 };
 
-exports.start = gulp.series(clean, gulp.parallel(styles, html), watch, serve);
+exports.start = gulp.series(clean, images, gulp.parallel(styles, html), watch, serve);
+
+exports.build = gulp.series(clean, images, gulp.parallel(styles, html));
