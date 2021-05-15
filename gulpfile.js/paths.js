5ac8c05e5769936b34507c2580448d0e0aba35f5
Alexander Repeta <alexander.repeta@gmail.com>
2021-05-15 13:44:17 +0300

    feat: add paths util

---
+++ gulpfile.js/paths.js
@@ -0,0 +1,28 @@
+module.exports = {
+  src: {
+    html: 'src/*.html',
+    css: 'src/sass/main.scss',
+    js: 'src/js/**/main.js',
+    images: 'src/img/**/*.{png,jpg,jpeg,webp,svg}',
+    fonts: 'src/fonts/**/*.{woff,woff2}',
+  },
+  watch: {
+    html: 'src/**/*.html',
+    css: 'src/sass/**/*.scss',
+    js: 'src/js/**/*.js',
+    images: 'src/img/**/*.{png,jpg,jpeg,webp,svg}',
+  },
+  dist: {
+    html: 'dist/',
+    css: 'dist/css',
+    js: 'dist/js',
+    images: 'dist/images',
+    fonts: 'dist/fonts',
+  },
+  inject: {
+    html: 'dist/index.html',
+    css: 'dist/css/**/*.css',
+    js: 'dist/js/**/*.js',
+  },
+  clean: 'dist/',
+};
