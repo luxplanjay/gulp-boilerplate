f1b3e55804e9e83f7d6757d0b321d02204ce415e
Alexander Repeta <alexander.repeta@gmail.com>
2021-05-15 13:43:54 +0300

    feat: add serve task

---
+++ gulpfile.js/serve.js
@@ -0,0 +1,20 @@
+/*
+ * BrowserSync web-server
+ */
+const browserSync = require('browser-sync').create();
+
+const serve = () => {
+  browserSync.init({
+    server: {
+      baseDir: './dist',
+    },
+    logPrefix: 'DevServer',
+    notify: false,
+    cors: true,
+    open: false,
+    ui: false,
+    port: process.env.PORT ?? 7070,
+  });
+};
+
+module.exports = serve;
