0640ee7e1909df196d3cb8ce6d2c18f27a65fc6d
Alexander Repeta <alexander.repeta@gmail.com>
2021-05-15 15:57:47 +0300

    fix: fix indentation

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -6,15 +6,17 @@ const { serve, browserSync } = require('./serve');
 const html = require('./html');
 const styles = require('./styles');
 const images = require('./images');
+const fonts = require('./fonts');
 const clean = require('./clean');
 
 const watch = done => {
     gulp.watch(paths.watch.html).on('change', gulp.series(html, browserSync.reload));
     gulp.watch(paths.watch.css).on('change', gulp.series(styles, browserSync.reload));
     gulp.watch(paths.watch.images, gulp.series(images, browserSync.reload));
+    gulp.watch(paths.watch.fonts, fonts);
     done();
 };
 
-exports.start = gulp.series(clean, images, gulp.parallel(styles, html), watch, serve);
+exports.start = gulp.series(clean, images, gulp.parallel(fonts, styles, html), watch, serve);
 
-exports.build = gulp.series(clean, images, gulp.parallel(styles, html));
+exports.build = gulp.series(clean, images, gulp.parallel(fonts, styles, html));
