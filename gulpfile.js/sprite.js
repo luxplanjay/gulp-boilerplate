fb29a28f787c2a420c2016e29802bba9756fc7a5
Alexander Repeta <alexander.repeta@gmail.com>
2021-05-16 01:22:35 +0300

    feat: add svgmin to sprite task

--- gulpfile.js/sprite.js
+++ gulpfile.js/sprite.js
@@ -1,7 +1,17 @@
 const gulp = require('gulp');
 const svgSprite = require('gulp-svg-sprite');
 const notify = require('gulp-notify');
+const svgmin = require('gulp-svgmin');
 const paths = require('./paths');
+
+const svgminOptions = {
+    plugins: [
+        { removeViewBox: false },
+        { cleanupIDs: false },
+        { removeComments: true },
+        { removeEmptyContainers: true },
+    ],
+};
 
 const svgSpriteOptions = {
     mode: {
@@ -14,6 +24,7 @@ const svgSpriteOptions = {
 const sprite = () => {
     return gulp
         .src(paths.src.sprite)
+        .pipe(svgmin(svgminOptions))
         .pipe(svgSprite(svgSpriteOptions).on('error', notify.onError()))
         .pipe(gulp.dest(paths.dist.images));
 };
