3ed15a06ac3cca466feb674126ae1f150c659f5a
Alexander Repeta <alexander.repeta@gmail.com>
2021-05-15 15:24:10 +0300

    feat: add images task

---
+++ gulpfile.js/images.js
@@ -0,0 +1,42 @@
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const newer = require('gulp-newer');
+const size = require('gulp-size');
+const webp = require('gulp-webp');
+const mode = require('gulp-mode')();
+const paths = require('./paths');
+
+const imageminOptions = {
+  mozjpeg: {
+    quality: 75,
+    progressive: true,
+  },
+  optipng: {
+    optimizationLevel: 5,
+  },
+  svgo: {
+    plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+  },
+};
+
+const images = () => {
+  return gulp
+    .src(paths.src.images)
+    .pipe(newer(paths.dist.images))
+    .pipe(webp({ quality: 75 }))
+    .pipe(gulp.dest(paths.dist.images))
+    .pipe(gulp.src(paths.src.images))
+    .pipe(
+      mode.production(
+        imagemin([
+          imagemin.mozjpeg(imageminOptions.mozjpeg),
+          imagemin.optipng(imageminOptions.optipng),
+          imagemin.svgo(imageminOptions.svgo),
+        ]),
+      ),
+    )
+    .pipe(size({ showFiles: true }))
+    .pipe(gulp.dest(paths.dist.images));
+};
+
+module.exports = images;
