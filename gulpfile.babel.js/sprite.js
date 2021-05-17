6257bedfc5f2d4e001da6da833c0fa021d6ad94f
Alexander Repeta <alexander.repeta@gmail.com>
2021-05-17 21:03:42 +0300

    feat: add useless svg attrs removal

--- gulpfile.babel.js/sprite.js
+++ gulpfile.babel.js/sprite.js
@@ -2,6 +2,7 @@ import gulp from 'gulp';
 import svgSprite from 'gulp-svg-sprite';
 import notify from 'gulp-notify';
 import svgmin from 'gulp-svgmin';
+import cheerio from 'gulp-cheerio';
 import paths from './paths';
 
 const svgminOptions = {
@@ -15,16 +16,28 @@ const svgminOptions = {
 
 const svgSpriteOptions = {
     mode: {
-        stack: {
+        symbol: {
             sprite: '../icons.svg',
         },
     },
+};
+
+const removeUselessAttrs = $ => {
+    $('[fill]').removeAttr('fill');
+    $('[stroke]').removeAttr('stroke');
+    $('[style]').removeAttr('style');
 };
 
 const sprite = () => {
     return gulp
         .src(paths.src.sprite)
         .pipe(svgmin(svgminOptions))
+        .pipe(
+            cheerio({
+                run: removeUselessAttrs,
+                parserOptions: { xmlMode: true },
+            }),
+        )
         .pipe(svgSprite(svgSpriteOptions).on('error', notify.onError()))
         .pipe(gulp.dest(paths.dist.images));
 };
